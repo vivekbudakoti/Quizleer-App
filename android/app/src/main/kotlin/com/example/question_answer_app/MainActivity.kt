22a1c958170abc60a41ac4bdf09c572fa074bd59
Vivek <thevivekbudakoti@gmail.com>
2021-08-31 07:57:57 +0530

    Quizller app

---
+++ android/app/src/main/kotlin/com/example/question_answer_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.question_answer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/questions.dart
@@ -0,0 +1,11 @@
+class Questions{
+  late String questionText;
+  late bool result;
+
+ Questions(String s, bool b){
+   questionText = s;
+   result = b;
+ }
+
+
+}
